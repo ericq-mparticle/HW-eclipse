6e4be1a681c14d098f5a872c1767fedc1f6ed5ce
ericq-mparticle <ericq@mparticle.com>
2016-07-14 12:33:07 -0400

    Added new method

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,8 +1,11 @@
-
 public class HelloWorld {
-
 	public static void main(String[] args) {
 		System.out.println("Hello, World!");
+		
+		someOtherMethod();
 	}
-
+	
+	private static void someOtherMethod(){
+		System.out.println("yo");
+	}
 }
